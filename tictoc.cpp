bb610975e5d41c1c375d6524e071ad566677332a
WilliamZhuo <aaa0025235@gmail.com>
2019-02-02 18:48:07 +0800

    Initial commit

---
+++ tictoc.cpp
@@ -0,0 +1,12 @@
+#include <chrono>
+
+using namespace std::chrono;
+int tictoc(int&timestamp)
+{
+	milliseconds ms = duration_cast<milliseconds>(
+		system_clock::now().time_since_epoch()
+		)
+	int timediff = (int)ms - timestamp;
+	timestamp = (int)ms;
+	return timediff;
+}
---
+++ tictoc.h
@@ -0,0 +1,2 @@
+#pragma once
+int tictoc(int&timestamp);
